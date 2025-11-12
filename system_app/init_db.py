7be68052d0d27f94d0a31aeb95b9482cafe84744
aAhmedbiram <154108436+aAhmedbiram@users.noreply.github.com>
2025-11-12 22:20:23 +0200

    Update init_db.py

--- system_app/init_db.py
+++ system_app/init_db.py
@@ -1,8 +1,11 @@
-# init_db.py
-from system_app.app import app  # استبدل المسار ده حسب مكان ملف app.py
-from system_app.models import db  # نفس الشيء: غيّر المسار حسب مكان models.py
+from system_app.app import app
+from system_app.queries import get_db, create_table, close_db
 
-# إنشاء كل الجداول داخل الكونتكست الخاص بـ Flask
 with app.app_context():
-    db.create_all()
-    print("✅ Tables created successfully in PostgreSQL!")
+    try:
+        create_table()
+        print("✅ Tables created successfully in PostgreSQL!")
+    except Exception as e:
+        print(f"❌ Error creating tables: {e}")
+    finally:
+        close_db()
