a0b704bd3c2f31cd998fbde0bbd9c03b6a477bf4
aAhmedbiram <ahmedbiram47@gmail.com>
2025-11-25 10:55:04 +0200

    fssd

--- system_app/app.py
+++ system_app/app.py
@@ -546,8 +546,8 @@ def send_email():
                     return render_template('send_email.html')
                 
                 # Email configuration
-                sender_email = 'ahmedbiram47@gmail.com'
-                sender_password = os.environ.get('GMAIL_APP_PASSWORD', 'ypvg tfbd zdru niow')
+                sender_email = 'rival.gym1@gmail.com'
+                sender_password = os.environ.get('GMAIL_APP_PASSWORD', 'kshbfyznimlxhiqr')
                 
                 if not sender_password:
                     flash('Email password not configured!', 'error')
